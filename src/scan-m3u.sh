6cf50411adab7dda53f04c6548aae5658aa9bb24
sybdata <as15@wt.de>
2018-04-01 23:30:54 +0200

    Create scan-m3u.sh

---
+++ src/scan-m3u.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FNAME="tv.m3u"
+URL="http://pomoyka.lib.emergate.net/trash/ttv-list/ttv.all.iproxy.m3u?ip=192.168.178.92:6878"
+
+GNAME="${FNAME}.gz"
+curl --fail -R -z "${GNAME}" -o "${GNAME}" -H "Accept-Encoding: gzip" "${URL}" && \
+ gunzip -c "${GNAME}" >"${FNAME}"
