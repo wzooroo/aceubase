a06cd08c85539f509895aa7de550e3a60388c8ac
sybdata <as15@wt.de>
2019-01-14 21:34:11 +0100

    Delete scan-m3u.sh

--- src/scan-m3u.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-FNAME="tv.m3u"
-URL="http://pomoyka.lib.emergate.net/trash/ttv-list/ttv.all.iproxy.m3u?ip=192.168.178.92:6878"
-
-GNAME="${FNAME}.gz"
-curl --fail -R -z "${GNAME}" -o "${GNAME}" -H "Accept-Encoding: gzip" "${URL}" && \
- gunzip -c "${GNAME}" >"${FNAME}"
