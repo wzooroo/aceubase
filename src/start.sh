bcb7a565cc552e6284568c09ed956f996b20f3bd
sybdata <as15@wt.de>
2019-01-30 19:36:01 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -4,5 +4,5 @@ exec python3 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
 while true; do
         sleep 600
-        rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
+        rm -rf /tmp/state/.ACEStream/collected_torrent_files/*
 done
