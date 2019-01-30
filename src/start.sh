d68daa9cc62ad53de383f5aa0aa8e7d0479c8571
sybdata <as15@wt.de>
2019-01-30 19:33:43 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exec /etc/init.d/supervisord start
+exec python3 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
+
+while true; do
+        sleep 600
+        rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
+done
