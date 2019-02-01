97b64f6094de2139943d15ac777e3e49adc9b018
sybdata <as15@wt.de>
2019-02-01 23:16:57 +0100

    Update aceconfig.py

--- root/opt/HTTPAceProxy-master/aceconfig.py
+++ root/opt/HTTPAceProxy-master/aceconfig.py
@@ -12,7 +12,7 @@ class AceConfig(acedefconfig.AceDefConfig):
     acespawn = True
     # Ace Stream cmd line (use `--log-file filepath` to write log)
     # You need to set it only on Linux based systems. Autodetect for Windows!
-    acecmd = '/opt/start-engine --client-console @/opt/acestream.conf --bind-all --cache-dir /tmp/state/.ACEStream --state-dir /tmp/state/.ACEStream --access-token 666666'
+    acecmd = '/opt/start-engine --client-console @/opt/acestream.conf --bind-all --cache-dir /tmp/state/.ACEStream --state-dir /tmp/state/.ACEStream --access-token 666666 --service-remote-access --stats-report-peers'
     # Ace Stream API key
     # You probably shouldn't touch this
     acekey = 'n51LvQoTlJzNGaFxseRK-uvnvX-sD4Vm5Axwmc4UcoD-jruxmKsuJaH0eVgE'
